51b809d10ed7ea8dcf51e8628829d2029a345fd3
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-09-06 18:04:14 -0300

    fix: StringHelper methods

---
+++ TesteDotNet/Calculadora/Utils/StringHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace TesteDotNet.Calculadora.Utils
+{
+    public static class StringHelpers
+    {
+        public static string[] FormatarEntrada(string input)
+        {
+            return input.Split(';').Select(sValue => sValue.Trim()).ToArray();
+        }
+    }
+}
