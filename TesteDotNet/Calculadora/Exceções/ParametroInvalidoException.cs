549e4a9c8cecacdc4eb2e802e11b48afdf96552c
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-09-06 18:04:02 -0300

    fix: Added parameter exception

---
+++ TesteDotNet/Calculadora/Exceções/ParametroInvalidoException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteDotNet.Calculadora.Exceções
+{
+    class ParametroInvalidoException : Exception
+    {
+        public ParametroInvalidoException() : base("Parametro invalido!") { }
+    }
+}
