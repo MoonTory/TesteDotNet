9bd6e1cb556af606d541d991f1b4e9557821baab
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-09-06 16:47:17 -0300

    fix: Class Calculadora

---
+++ TesteDotNet/Calculadora/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using TesteDotNet.Calculadora.Exceções;
+
+namespace TesteDotNet.Calculadora
+{
+    public class Calculadora
+    {
+        public bool isRunning;
+        public Calculadora()
+        {
+            isRunning = true;
+        }
+
+        public void Init()
+        {
+            Console.WriteLine("Sejam bem vindos!!!");
+            while (isRunning)
+            {
+
+            }
+            Exit();
+        }
+
+        public double Soma(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtrair(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiplcar(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Dividir(double a, double b)
+        {
+            if (b == 0)
+                throw new DividirPorZeroException(a, b);
+
+            return a / b;
+        }
+
+        private void Exit()
+        {
+
+            Thread.Sleep(2000);
+        }
+    }
+}
