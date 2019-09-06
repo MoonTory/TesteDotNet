d381a8008566730e8f8dc4f9e19b9baa14f75587
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-09-06 18:59:12 -0300

    fix: Invalid Operation Exception

---
+++ TesteDotNet/Calculadora/Exceções/OperacaoInvalidoException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteDotNet.Calculadora.Exceções
+{
+    public class OperacaoInvalidoException : Exception
+    {
+        public OperacaoInvalidoException() : base("Operacao Invalido!") { }
+    }
+}
