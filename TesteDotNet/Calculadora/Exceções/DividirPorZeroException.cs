a68de55b9cf970d43d6ca83f8b9818a85a80904c
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-09-06 16:47:35 -0300

    fix: Divide by zero exception

---
+++ TesteDotNet/Calculadora/Exceções/DividirPorZeroException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TesteDotNet.Calculadora.Exceções
+{
+    public class DividirPorZeroException : Exception
+    {
+        public DividirPorZeroException(double a, double b) : base($"Divisão por zero é proibido!: Valor A: {a}, Valor B: {b}") { }
+    }
+}
